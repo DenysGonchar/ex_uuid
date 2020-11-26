5294aa66be30d4108a3943d3d960879c44e098c6
DenysGonchar <denys.gonchar@erlang-solutions.com>
2020-11-26 22:54:52 +0100

    an empty project

---
+++ lib/ex_uuid.ex
@@ -0,0 +1,18 @@
+defmodule ExUuid do
+  @moduledoc """
+  Documentation for `ExUuid`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExUuid.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_uuid_test.exs
@@ -0,0 +1,8 @@
+defmodule ExUuidTest do
+  use ExUnit.Case
+  doctest ExUuid
+
+  test "greets the world" do
+    assert ExUuid.hello() == :world
+  end
+end
